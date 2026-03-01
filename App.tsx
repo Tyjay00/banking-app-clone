47a7eb3da65b316005ff60382eabb7d9f69b617e
Tyrone Joel <tyronejoel04@gmail.com>
2026-03-01 13:18:03 +0200

    Update 7 Day Notice pending withdrawal date to 04 Mar 2026 09:00

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ import { UserState, Account, Transaction, Beneficiary, Bill } from './types';
 const MOCK_ACCOUNTS: Account[] = [
   { id: '1', type: 'Savings', balance: 0, accountNumber: 'Main Account', color: '#009fe3' },
   { id: '2', type: 'Investment', balance: 0, accountNumber: 'Savings Plans', color: '#004b91' },
-  { id: '3', type: '7 Day Notice', balance: 9500, accountNumber: '7 Day Notice Account', color: '#e20613', pendingWithdrawal: { amount: 9500, dueDate: '28 Feb 2026 14:42' } }
+  { id: '3', type: '7 Day Notice', balance: 9500, accountNumber: '7 Day Notice Account', color: '#e20613', pendingWithdrawal: { amount: 9500, dueDate: '04 Mar 2026 09:00' } }
 ];
 
 const MOCK_BENEFICIARIES: Beneficiary[] = [
